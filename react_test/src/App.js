a84d1a85f8d9f81251b347bba45f2a08ff746a0e
sujinpark6774 <psujin6774@naver.com>
2022-08-08 16:48:41 +0900

    commit_0808

--- react_test/src/App.js
+++ react_test/src/App.js
@@ -8,8 +8,8 @@ import CreateContent from './components/CreateContent';
 import UpdateContent from './components/UpdateContent';
 import './App.css';
 
-// git
 class App extends Component {
+
   constructor(props) {
     super(props);
     this.max_content_id = 3;
@@ -26,60 +26,33 @@ class App extends Component {
     }
   }
 
-  getContent() {
-    var _title, _desc, _article = null;
-    if(this.state.mode === 'welcome') {
-      _title = this.state.welcome.title;
-      _desc = this.state.welcome.desc;
-      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
-    } else if(this.state.mode === 'read') {
-      var i = 0;
-      while(i < this.state.contents.length) {
-        var data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id) {
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-        i = i + 1;
+  getReadContent() {
+    var i = 0;
+    while(i < this.state.contents.length) {
+      var data = this.state.contents[i];
+      if(data.id === this.state.selected_content_id) {
+        return data;
       }
-      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
-    } else if(this.state.mode === 'create') {
-      _article = <CreateContent onSubmit={function(_title, _desc) {
-        this.max_content_id = this.max_content_id + 1;
-        var _contents = this.state.contents.concat(
-          {id : this.max_content_id, title : _title, desc : _desc}
-        );
-        this.setState({
-          contents : _contents
-        });
-        console.log(_title, _desc);
-      }.bind(this)}></CreateContent>
-    } else {   ///////////////////////////////////////////////////여기부터 작성하기
-
+      i = i + 1;
     }
   }
 
-  render() {
+  getContent() {
 
     var _title, _desc, _article = null;
+
     if(this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+
     } else if(this.state.mode === 'read') {
-      var i = 0;
-      while(i < this.state.contents.length) {
-        var data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id) {
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-        i = i + 1;
-        _article = <ReadContent title={_title} desc={_desc}></ReadContent>
-      }
+
+      var _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} _desc={_content.desc}></ReadContent>
+
     } else if(this.state.mode === 'create') {
+
       _article = <CreateContent onSubmit={function(_title, _desc) {
         this.max_content_id = this.max_content_id + 1;
         var _contents = this.state.contents.concat(
@@ -90,8 +63,30 @@ class App extends Component {
         });
         console.log(_title, _desc);
       }.bind(this)}></CreateContent>
-    }
 
+    } else if(this.state.mode === 'update') {
+
+        _content = this.getReadContent();
+        _article = <UpdateContent data={_content} onSubmit = {function(_id, _title, _desc) {
+          var _contents = Array.from(this.state.contents);
+          var i = 0;
+          while(i < _content.length) {
+            if(_content[i].id === _id) {
+              _content[i] = {id : _id, title : _title, desc : _desc};
+              break;
+            }
+            i = i + 1;
+          }
+          this.setState({
+            contents : _contents
+          });
+        }.bind(this)}></UpdateContent>
+    }
+    return _article;
+  }
+
+  render() {
+    console.log('App render');
     return (
       <div className='App'>
 
@@ -121,12 +116,10 @@ class App extends Component {
           }.bind(this)}
         ></Control>
 
-        {_article}
-
-        <ReadContent title={_title} desc={_desc}></ReadContent>
+        {this.getContent()}
 
       </div>
-    )
+    );
   }
 }
 
