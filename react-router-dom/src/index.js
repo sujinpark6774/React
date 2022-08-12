3510e8edecf783a09ed90312d079059407235874
sujinpark6774 <psujin6774@naver.com>
2022-08-12 15:37:47 +0900

    04. Link _ 0812

--- react-router-dom/src/index.js
+++ react-router-dom/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch, NavLink } from 'react-router-dom';
 
 function Home() {
   return (
@@ -35,10 +35,11 @@ function App() {
   return (
     <div>
       <h1>Hello React Router DOM</h1>
+
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/topics">Topics</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><NavLink exact to="/">Home</NavLink></li>
+        <li><NavLink to="/topics">Topics</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
       </ul>
 
       <Switch>
@@ -52,7 +53,7 @@ function App() {
   )
 }
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
