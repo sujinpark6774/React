38fadc6593fed20a02b3589fb631d66c2b2427b4
sujinpark6774 <psujin6774@naver.com>
2022-08-12 14:04:08 +0900

    05. react life cycle_0812

--- react-func-class-style-coding/src/App.js
+++ react-func-class-style-coding/src/App.js
@@ -1,26 +1,69 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 
 function App() {
+  var [funcShow, setfuncShow] = useState(true);
+  var [classShow, setclassShow] = useState(true);
+
   return (
     <div className="container">
       <h1>Hello World</h1>
-      <FuncComp initNumber={2}></FuncComp>
-      <ClassComp initNumber={2}></ClassComp>
+      <input type="button" value="remove func" onClick={
+        function() {
+          setfuncShow(false);
+        }
+      }></input>
+      <input type="button" value="remove class" onClick={
+        function() {
+          setclassShow(false);
+        }
+      }></input>
+
+      {funcShow ? <FuncComp initNumber={2}></FuncComp> : null}
+      {classShow ? <ClassComp initNumber={2}></ClassComp> : null}
     </div>
   );
+
 }
 
 
 // 1. function을 이용한 컴포넌트 생성
+var funcStyle = 'color:blue';
+var funcId = 0;
+
 function FuncComp(props) {
   var numberState = useState(props.initNumber);
   var number = numberState[0];
   var setNumber = numberState[1];
 
   var [_date, setDate] = useState((new Date()).toString());
+  
+  useEffect(function() {
+    console.log('%cfunc => useEffect (componentDidMount) '+(++funcId), funcStyle);
+    document.title = number;
+    return function() {
+      console.log('%cfunc => useEffect return (componentWillUnMount) ' +(++funcId), funcStyle);
+    }
+  }, []);
 
-  console.log('numberState', numberState);
+  // side effect
+  useEffect(function() {
+    console.log('%cfunc => useEffect number (componentDidMount & componentDidUpdate) '+(++funcId), funcStyle);
+    document.title = number;
+    return function() {
+      console.log('%cfunc => useEffect number return (componentDidMount & componentDidUpdate ' +(++funcId), funcStyle);
+    }
+  }, [number]);
+
+  useEffect(function() {
+    console.log('%cfunc => useEffect _date (componentDidMount & componentDidUpdate) '+(++funcId), funcStyle);
+    document.title = _date;
+    return function() {
+      console.log('%cfunc => useEffect _date return (componentDidMount & componentDidUpdate ' +(++funcId), funcStyle);
+    }
+  }, [_date]);
+
+  console.log('%cfunc => render '+(++funcId), funcStyle);
 
   return (
     <div className="container">
@@ -43,10 +86,33 @@ function FuncComp(props) {
 
 
 // 2. class를 이용한 컴포넌트 생성
+var classStyle = 'color:red';
+
 class ClassComp extends React.Component {
   state = {
     number : this.props.initNumber,
     date : (new Date()).toString()
+  }
+
+  UNSAFE_componentWillMount() {
+    console.log('%cclass => componentWillMount', classStyle)
+  }
+
+  componentDidMount() {
+    console.log('%cclass => componentDidMount', classStyle)
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('%cclass => shouldComponentUpdate', classStyle);
+    return true;
+  }
+
+  UNSAFE_componentWillUpdate(nextProps, nextState) {
+    console.log('%cclass => componentWillUpdate', classStyle);
+  }
+
+  componentDidUpdate(nextProps, nextState) {
+    console.log('%cclass => componentDidUpdate', classStyle);
   }
 
   render() {
