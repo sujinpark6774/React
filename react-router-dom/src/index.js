b69990a41820507f5195196895d131a1628fbff9
sujinpark6774 <psujin6774@naver.com>
2022-08-12 16:51:34 +0900

    05. Nested Routing _ 0812

--- react-router-dom/src/index.js
+++ react-router-dom/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter, Route, Switch, NavLink } from 'react-router-dom';
+import { HashRouter, Route, Switch, NavLink, useParams } from 'react-router-dom';
 
 function Home() {
   return (
@@ -13,11 +13,55 @@ function Home() {
   );
 }
 
+var contents = [
+  {id : 1, title : "HTML", description : "HTML is ..."},
+  {id : 2, title : "JS", description : "JavaScript is ..."},
+  {id : 3, title : "React", description : "React is ..."}
+];
+
+function Topic() {
+  var params = useParams();
+  var topic_id = params.topic_id;
+  var selected_topic = {
+    title : 'Sorry',
+    description : 'Not Found'
+  };
+
+  for (var i = 0; i < contents.length; i++) {
+    if(contents[i].id === Number(topic_id)) {
+      selected_topic = contents[i];
+      break;
+    }
+  }
+  console.log('params : ', params);
+
+  return (
+    <div>
+      <h3>{selected_topic.title}</h3>
+      {selected_topic.description}
+    </div>
+  );
+}
+
 function Topics() {
+  var lis = []
+  for (var i = 0; i < contents.length; i++) {
+    lis.push(
+      <li key={contents[i].id}><NavLink to={"/topics/" + contents[i].id}>{contents[i].title}</NavLink></li>
+    )
+  }
+
   return (
     <div>
       <h2>Topics</h2>
-      Topics...
+      
+      <ul>
+        {lis}
+      </ul>
+
+      <Route path="/topics/:topic_id">
+        <Topic></Topic>
+      </Route>
     </div>
   );
 }
